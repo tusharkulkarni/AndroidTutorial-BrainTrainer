e97602ed1aebb2aa2ab7c032290ccbd3233e584f
tusharkulkarni <tusharkulkarni1990@gmail.com>
2016-06-30 18:20:46 +0530

    Working app

--- app/src/main/java/com/tushar/braintrainer/MainActivity.java
+++ app/src/main/java/com/tushar/braintrainer/MainActivity.java
@@ -1,22 +1,108 @@
 package com.tushar.braintrainer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button  startButton;
+    TextView timerText;
+    TextView questionText;
+    Random rand = new Random();
+    int correctAnswer;
+    int totalCount ;
+    int correctCount ;
+    CountDownTimer countDownTimer;
+    Button  button1;
+    Button  button2;
+    Button  button3;
+    Button  button4;
+    Button playAgainButton;
+    TextView resultText;
+    TextView scoreText;
 
     public void start(View view){
         startButton.setVisibility(View.INVISIBLE);
+        scoreText.setVisibility(View.VISIBLE);
+        timerText.setVisibility(View.VISIBLE);
+        questionText.setVisibility(View.VISIBLE);
+        scoreText.setVisibility(View.VISIBLE);
+        resultText.setVisibility(View.VISIBLE);
+        GridLayout answerGrid = (GridLayout)findViewById(R.id.answersGrid);
+        answerGrid.setVisibility(View.VISIBLE);
+        reset();
+        newQuestion();
 
+    }
+    public void init(){
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+        scoreText = (TextView) findViewById(R.id.scoreText);
+        timerText = (TextView) findViewById(R.id.timerText);
+        startButton = (Button)findViewById(R.id.startButton);
+        questionText = (TextView)findViewById(R.id.questionText);
+        playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        resultText = (TextView)findViewById(R.id.resultText);
+    }
+
+    public void reset(){
+
+        totalCount = 0;
+        correctCount = 0;
+        correctAnswer = -1;
+        startButton.setVisibility(View.INVISIBLE);
+        resultText.setText("");
+        playAgainButton.setVisibility(View.INVISIBLE);
+        button1.setEnabled(true);
+        button2.setEnabled(true);
+        button3.setEnabled(true);
+        button4.setEnabled(true);
+        scoreText.setText(correctCount + "/" + totalCount);
+        countDownTimer = new CountDownTimer(30100, 990) {
+
+            public void onTick(long millisUntilFinished) {
+                long remTime = millisUntilFinished / 1000;
+                String remTimeString = "";
+                if(remTime>9){
+                    remTimeString = remTime + "s";
+                }else{
+                    remTimeString = "0" + remTime + "s";
+                }
+                timerText.setText(remTimeString);
+            }
+
+            public void onFinish() {
+                timerText.setText("00s");
+                countDownTimer.cancel();
+                resultText.setText("Your Score : " + correctCount + "/" + totalCount);
+                button1.setEnabled(false);
+                button2.setEnabled(false);
+                button3.setEnabled(false);
+                button4.setEnabled(false);
+                playAgainButton.setVisibility(View.VISIBLE);
+
+            }
+        }.start();
     }
 
     @Override
@@ -34,7 +120,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        startButton = (Button)findViewById(R.id.startButton);
+        init();
+
     }
 
     @Override
@@ -59,5 +146,56 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void newQuestion(){
+        int val1 = rand.nextInt(21);
+        int val2 = rand.nextInt(21);
+        questionText.setText(val1 + " + " + val2);
 
+        ArrayList<Integer> answerList = new ArrayList<Integer>();
+        correctAnswer = val1 + val2;
+
+        answerList.add(Integer.valueOf(correctAnswer));
+        answerList.add(getRandomAnswer(answerList));
+        answerList.add(getRandomAnswer(answerList));
+        answerList.add(getRandomAnswer(answerList));
+        Collections.shuffle(answerList);
+
+        button1.setText(answerList.get(0).toString());
+        button2.setText(answerList.get(1).toString());
+        button3.setText(answerList.get(2).toString());
+        button4.setText(answerList.get(3).toString());
+    }
+
+    public Integer getRandomAnswer(List<Integer> answerList){
+        Integer newAnswer = -1;
+        while(true){
+            newAnswer = Integer.valueOf(rand.nextInt(21) + rand.nextInt(21));
+            if(!answerList.contains(newAnswer)){
+                break;
+            }
+        }
+        return newAnswer;
+    }
+
+    public void submitAnswer(View view){
+        Button selectedButtonr = (Button)view;
+        int selectedAnswer = Integer.parseInt(selectedButtonr.getText().toString());
+        if(selectedAnswer == correctAnswer){
+            resultText.setText("Correct :)");
+            correctCount++;
+        }else{
+            resultText.setText("Incorrect :(");
+        }
+        totalCount++;
+
+        scoreText.setText(correctCount + "/" + totalCount);
+        newQuestion();
+    }
+
+    public void playAgain(View view){
+        playAgainButton.setVisibility(View.INVISIBLE);
+        reset();
+        newQuestion();
+
+    }
 }
